6e1d2903470e26894322a3f97c5f74f5bd201ae7
SimonHotDog <simon.lu@live.se>
2022-09-30 16:44:15 +0200

    Completed FizzBuzz

---
+++ C++/fizzbuzz/fizzbuzz.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    int x, y, n;
+
+    std::cin >> x >> y >> n;
+
+    for (int i = 1; i <=n; ++i) {
+        if(i % x != 0 && i % y != 0) {
+            std::cout << i << "\n";
+            continue;
+        }
+        if(i % x == 0) {
+            std::cout << "Fizz";
+        }
+        if(i % y == 0) {
+            std::cout << "Buzz";
+        }
+        std::cout << "\n";
+    }
+}
